b2b248b8dd590ba4b723b59c6061150a6451edb2
Andy Miller <andy@andyfmiller.com>
2018-09-16 16:04:37 -0700

    Display some of the resource link request.

--- src/Pages/Tool.cshtml.cs
+++ src/Pages/Tool.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using LtiLibrary.NetCore.Common;
+using LtiLibrary.NetCore.Lti.v1p3;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,28 +10,58 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace AdvantageTool.Pages
 {
-    // Tool launches typically come from outsite this app. Order not required starting with AspNetCore 2.2.
+    // Tool launches typically come from outsite this app. Order will not be required starting with AspNetCore 2.2.
     // See https://github.com/aspnet/Mvc/issues/7795#issuecomment-397071059
     [IgnoreAntiforgeryToken(Order = 1001)]
     public class ToolModel : PageModel
     {
-        // Step 5 in the OpenId Implicit Flow
-        // See https://www.imsglobal.org/spec/security/v1p0/#platform-originating-messages
-        // See https://openid.net/specs/openid-connect-core-1_0.html#ImplicitFlowSteps
+        /// <summary>
+        /// Get or set the error discovered while parsing the request.
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// Get or set the id_token in the request.
+        /// </summary>
+        [BindProperty(Name = "id_token")]
+        public string IdToken { get; set; }
+
+        public LtiResourceLinkRequest LtiRequest { get; set; }
+
+        /// <summary>
+        /// Get or set the JwtSecurityToken (id_token) in the request.
+        /// </summary>
+        public JwtSecurityToken Token { get; set; }
+
+        /// <summary>
+        /// Handle the LTI POST request to launch the tool. 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
         public IActionResult OnPost()
         {
+            // Authenticate the request starting at step 5 in the OpenId Implicit Flow
+            // See https://www.imsglobal.org/spec/security/v1p0/#platform-originating-messages
+            // See https://openid.net/specs/openid-connect-core-1_0.html#ImplicitFlowSteps
+
             // The Platform MUST send the id_token via the OAuth 2 Form Post
             // See https://www.imsglobal.org/spec/security/v1p0/#successful-authentication
             // See http://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html
-            
+
             if (string.IsNullOrEmpty(IdToken))
-                return new BadRequestResult();
+            {
+                Error = "id_token is missing or empty";
+                return Page();
+            }
 
             var handler = new JwtSecurityTokenHandler();
             if (!handler.CanReadToken(IdToken))
-                return new BadRequestResult();
-            
-            var token = handler.ReadJwtToken(IdToken);
+            {
+                Error = "Cannot read id_token";
+                return Page();
+            }
+
+            Token = handler.ReadJwtToken(IdToken);
 
             // Authentication Response Validation
             // See https://www.imsglobal.org/spec/security/v1p0/#authentication-response-validation
@@ -39,14 +71,29 @@ namespace AdvantageTool.Pages
             // Issuer Identifier was collected in an offline process.
             // See https://www.imsglobal.org/spec/security/v1p0/#dfn-issuer-identifier
             
-            var issuer = token.Issuer; // Normally this would actually look it up!
-            if (issuer == null)
-                return new UnauthorizedResult();
+            if (string.IsNullOrEmpty(Token.Issuer))
+            {
+                Error = "Issuer is missing from id_token";
+                return Page();
+            }
+
+            // Look for the local record of this issuer
+            
+            var issuer = Token.Issuer; // Normally this would actually look up the issuer
+            if (string.IsNullOrEmpty(issuer))
+            {
+                Error = $"Issuer '{issuer}' is not recognized.";
+                return Page();
+            }
 
             // The ID Token MUST contain a nonce Claim.
-            var nonce = token.Claims.SingleOrDefault(c => c.Type == "nonce")?.Value;
+
+            var nonce = Token.Claims.SingleOrDefault(c => c.Type == "nonce")?.Value;
             if (string.IsNullOrEmpty(nonce))
-                return new UnauthorizedResult();
+            {
+                Error = "Nonce is missing from id_token";
+                return Page();
+            }
 
             // Using the JwtSecurityTokenHandler.ValidateToken method, validate four things:
             //
@@ -97,171 +144,58 @@ oQIDAQAB
             {
                 handler.ValidateToken(IdToken, validationParameters, out _);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                return new UnauthorizedResult();
+                Error = e.Message;
+                return Page();
+            }
+
+            try
+            {
+                LtiRequest = new LtiResourceLinkRequest(Token.Payload);
+            }
+            catch (LtiException e)
+            {
+                Error = e.Message;
             }
 
             return Page();
         }
 
-        [BindProperty(Name = "id_token")] public string IdToken { get; set; }
-
-        //private RsaSecurityKey GetRsaSecurityKey(string key)
+        //private LtiRequest GetLtiRequest(JwtSecurityToken token)
         //{
-        //    var x509Key = DecodeOpenSslPublicKey(key);
-        //    var rsaCryptoServiceProvider = DecodeX509PublicKey(x509Key);
-        //    return new RsaSecurityKey(rsaCryptoServiceProvider);
-        //}
-
-        //private static byte[] DecodeOpenSslPublicKey(string instr)
-        //{
-        //    const string pempubheader = "-----BEGIN PUBLIC KEY-----";
-        //    const string pempubfooter = "-----END PUBLIC KEY-----";
-        //    string pemstr = instr.Trim();
-        //    byte[] binkey;
-        //    if (!pemstr.StartsWith(pempubheader) || !pemstr.EndsWith(pempubfooter))
-        //        return null;
-        //    var sb = new StringBuilder(pemstr);
-        //    sb.Replace(pempubheader, ""); //remove headers/footers, if present
-        //    sb.Replace(pempubfooter, "");
-
-        //    string pubstr = sb.ToString().Trim(); //get string after removing leading/trailing whitespace
-
-        //    try
+        //    var messageType = token.Claims
+        //        .SingleOrDefault(c => c.Type == "https://purl.imsglobal.org/spec/lti/claim/message_type")?.Value;
+        //    if (string.IsNullOrEmpty(messageType))
         //    {
-        //        binkey = Convert.FromBase64String(pubstr);
-        //    }
-        //    catch (FormatException)
-        //    {
-        //        //if can't b64 decode, data is not valid
-        //        return null;
+        //        throw new LtiException("message_type is missing from token.");
         //    }
 
-        //    return binkey;
-        //}
-
-        //private static RSACryptoServiceProvider DecodeX509PublicKey(byte[] x509Key)
-        //{
-        //    // encoded OID sequence for  PKCS #1 rsaEncryption szOID_RSA_RSA = "1.2.840.113549.1.1.1"
-        //    byte[] seqOid = {0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00};
-        //    // ---------  Set up stream to read the asn.1 encoded SubjectPublicKeyInfo blob  ------
-        //    using (var mem = new MemoryStream(x509Key))
+        //    var version = token.Claims
+        //        .SingleOrDefault(c => c.Type == "https://purl.imsglobal.org/spec/lti/claim/version")?.Value;
+        //    if (string.IsNullOrEmpty(version))
         //    {
-        //        using (var binr = new BinaryReader(mem)) //wrap Memory Stream with BinaryReader for easy reading
-        //        {
-        //            try
-        //            {
-        //                var twobytes = binr.ReadUInt16();
-        //                switch (twobytes)
-        //                {
-        //                    case 0x8130:
-        //                        binr.ReadByte(); //advance 1 byte
-        //                        break;
-        //                    case 0x8230:
-        //                        binr.ReadInt16(); //advance 2 bytes
-        //                        break;
-        //                    default:
-        //                        return null;
-        //                }
-
-        //                var seq = binr.ReadBytes(15);
-        //                if (!CompareBytearrays(seq, seqOid)) //make sure Sequence for OID is correct
-        //                    return null;
-
-        //                twobytes = binr.ReadUInt16();
-        //                if (twobytes == 0x8103
-        //                ) //data read as little endian order (actual data order for Bit String is 03 81)
-        //                    binr.ReadByte(); //advance 1 byte
-        //                else if (twobytes == 0x8203)
-        //                    binr.ReadInt16(); //advance 2 bytes
-        //                else
-        //                    return null;
-
-        //                var bt = binr.ReadByte();
-        //                if (bt != 0x00) //expect null byte next
-        //                    return null;
-
-        //                twobytes = binr.ReadUInt16();
-        //                if (twobytes == 0x8130
-        //                ) //data read as little endian order (actual data order for Sequence is 30 81)
-        //                    binr.ReadByte(); //advance 1 byte
-        //                else if (twobytes == 0x8230)
-        //                    binr.ReadInt16(); //advance 2 bytes
-        //                else
-        //                    return null;
-
-        //                twobytes = binr.ReadUInt16();
-        //                byte lowbyte;
-        //                byte highbyte = 0x00;
-
-        //                if (twobytes == 0x8102
-        //                ) //data read as little endian order (actual data order for Integer is 02 81)
-        //                    lowbyte = binr.ReadByte(); // read next bytes which is bytes in modulus
-        //                else if (twobytes == 0x8202)
-        //                {
-        //                    highbyte = binr.ReadByte(); //advance 2 bytes
-        //                    lowbyte = binr.ReadByte();
-        //                }
-        //                else
-        //                    return null;
-
-        //                byte[] modint =
-        //                    {lowbyte, highbyte, 0x00, 0x00}; //reverse byte order since asn.1 key uses big endian order
-        //                int modsize = BitConverter.ToInt32(modint, 0);
-
-        //                byte firstbyte = binr.ReadByte();
-        //                binr.BaseStream.Seek(-1, SeekOrigin.Current);
-
-        //                if (firstbyte == 0x00)
-        //                {
-        //                    //if first byte (highest order) of modulus is zero, don't include it
-        //                    binr.ReadByte(); //skip this null byte
-        //                    modsize -= 1; //reduce modulus buffer size by 1
-        //                }
-
-        //                byte[] modulus = binr.ReadBytes(modsize); //read the modulus bytes
-
-        //                if (binr.ReadByte() != 0x02) //expect an Integer for the exponent data
-        //                    return null;
-        //                int expbytes =
-        //                    binr.ReadByte(); // should only need one byte for actual exponent data (for all useful values)
-        //                byte[] exponent = binr.ReadBytes(expbytes);
-
-        //                // We don't really need to print anything but if we insist to...
-        //                //showBytes("\nExponent", exponent);
-        //                //showBytes("\nModulus", modulus);
-
-        //                // ------- create RSACryptoServiceProvider instance and initialize with public key -----
-        //                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-        //                RSAParameters rsaKeyInfo = new RSAParameters
-        //                {
-        //                    Modulus = modulus,
-        //                    Exponent = exponent
-        //                };
-        //                rsa.ImportParameters(rsaKeyInfo);
-        //                return rsa;
-        //            }
-        //            catch (Exception)
-        //            {
-        //                return null;
-        //            }
-        //        }
+        //        throw new LtiException("version is missing from token.");
         //    }
-        //}
 
-        //static bool CompareBytearrays( byte[] a, byte[] b )
-        //{
-        //    if (a.Length != b.Length)
-        //        return false;
-        //    int i = 0;
-        //    foreach (byte c in a)
+        //    var deploymentId = token.Claims
+        //        .SingleOrDefault(c => c.Type == "https://purl.imsglobal.org/spec/lti/claim/deployment_id")?.Value;
+        //    if (string.IsNullOrEmpty(version))
         //    {
-        //        if (c != b[i])
-        //            return false;
-        //        i++;
+        //        throw new LtiException("deployment_id is missing from token.");
         //    }
-        //    return true;
+
+        //    var request = new LtiRequest(messageType)
+        //    {
+        //        DeploymentId = deploymentId,
+        //        LtiVersion = version
+        //    };
+
+        //    request.LisPersonNameFamily = token.GetClaimValueAsString("family_name");
+        //    request.LisPersonNameGiven = token.GetClaimValueAsString("given_name");
+        //    request.LisPersonNameMiddle = token.GetClaimValueAsString("middle_name");
+
+        //    return request;
         //}
     }
 }
