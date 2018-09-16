4bbee131ab1ce7bb41be8d9b3a3e44437a4bdeba
Andy Miller <andy@andyfmiller.com>
2018-09-15 22:33:40 -0700

    Cleanup

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AdvantageTool.Data
 {
