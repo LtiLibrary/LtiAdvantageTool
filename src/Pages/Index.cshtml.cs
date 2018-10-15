b08cf9db95e61eaa4e58296e29d3d7f9ddeb7b46
Andy Miller <andy@andyfmiller.com>
2018-10-14 17:56:06 -0700

    Create a home page

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvantageTool.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdvantageTool.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
+        public IList<Client> Clients { get; set; }
+        public IList<Platform> Platforms { get; set; }
+
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            Clients = await _context.Clients
+                .Where(c => c.UserId == user.Id)
+                .ToListAsync();
+            Platforms = await _context.Platforms
+                .Where(p => p.UserId == user.Id)
+                .ToListAsync();
         }
     }
 }
