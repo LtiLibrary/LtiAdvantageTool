54f9d4932c1ee2840fa8d07c6c4084e304ef34f2
Andy Miller <andy@andyfmiller.com>
2018-11-20 20:13:23 -0800

    Simplify passwords.

--- src/Startup.cs
+++ src/Startup.cs
@@ -34,7 +34,14 @@ namespace AdvantageTool
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<AdvantageToolUser>()
+            services.AddDefaultIdentity<AdvantageToolUser>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength = 4;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.ConfigureApplicationCookie(options => options.Cookie.Name = "AdvantageTool" );
