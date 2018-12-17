028437923ed0f01c6324852d33656196c06ef0ba
Andy Miller <andy@andyfmiller.com>
2018-12-16 23:54:18 -0800

    Install application insights.

--- src/Program.cs
+++ src/Program.cs
@@ -12,6 +12,7 @@ namespace AdvantageTool
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
