d7ad28889becb1fdb60da92926f14b2f31c3cfd8
Sander Rijken <sr@d-90.nl>
2026-05-03 19:57:03 +0200

    Scaffold fresh net10.0 webapp with Identity + SQLite

---
+++ src/AdvantageTool/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvantageTool.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
